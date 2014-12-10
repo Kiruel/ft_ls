9c431b26cadb974530eed9818dc5607589daf6dd
Etienne THEODORE <etheodor@e2r5p10.42.fr>
2014-12-10 16:36:08 +0100

    resolve -R and -Ra for see hidden folder or not

--- ft_print_list.c
+++ ft_print_list.c
@@ -12,19 +12,46 @@
 
 #include "includes/ft_ls.h"
 
+int		ft_i_strrchr(const char *s, int c)
+{
+	int i;
+
+	i = (int)ft_strlen((char*)s);
+	while (i >= 0)
+	{
+		if ((char)s[i] == (char)c)
+			return (i);
+		i--;
+	}
+	return (0);
+}
+
 void	ft_print_list(t_data *list, char *opt, char *path, int k)
 {
 	t_data	*tmp2;
 	int 	*size;
-	char	*ret;
+	int		i;
+	int 	j;
 
 	size = ft_count_space(list, opt);
 	tmp2 = list;
-	ret = ft_strrchr(path, '/');
+	i = 0;
+	j = ft_strlen(path);
+	i = ft_i_strrchr(path, '/');
+	i++;
+	while (j > i)
+		j--;
 	while (tmp2 != NULL)
 	{
-		// if (ret[1] != '.')
+		if (opt[2] != 'a')
+		{
+			if (path[j] != '.')
+				ft_print_l(tmp2, opt, size);
+		}
+		else
+		{
 			ft_print_l(tmp2, opt, size);
+		}
 		tmp2 = tmp2->next;
 	}
 	free(size);
